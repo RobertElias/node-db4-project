7fa25babf7d7529725a56c2c3ac2445e31880ed3
robertelias <robert.elias@live.com>
2020-05-14 15:00:09 -0700

    recipe.js completed

--- data/migtrations/20200514142939_recipe.js
+++ data/migtrations/20200514142939_recipe.js
@@ -1,8 +1,49 @@
-
 exports.up = function(knex) {
+    return knex.schema.createTable('recipes', tbl => {
+        tbl.increments();
+        tbl.string('recipename', 128).notNullable();
+        tbl.text('steps');
+    })
+    .createTable('ingredients', tbl => {
+        tbl.increments();
+        tbl.string('ingredient', 128).notNullable();
+    })
+    .createTable('recipesteps', tbl => {
+          tbl.increments();
+          tbl.string('stepdescription').notNullable();
+          tbl.integer('recipeId')
+              .unsigned()
+              .notNullable()
+              .references('id')
+              .inTable('recipes');
+          tbl.integer('steporder')
+          .notNullable()
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE');
+    })
+    .createTable('recipeingredients', tbl => {
+          tbl.increments();
+          tbl.integer('recipeId')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('recipes');
+          tbl.integer('ingredientId')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('ingredients');
+          tbl.float('measurement').notNullable();
+          tbl.string('measurementType').notNullable();  // t, T, Cup(s)
+          tbl.integer('ingredientOrder')
+    })
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex) {
+      return knex.schema
+      .dropTableIfExists('recipes')
+      .dropTableIfExists('ingredients')
+      .dropTableIfExists('recipesteps')
+      .dropTableIfExists('recipeingredients');
+    
+  };
